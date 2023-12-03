76606e361344b6b6f08c380cb37945e703ecbd0d
Chai <7232280+Chailotl@users.noreply.github.com>
2023-12-03 01:30:34 -0500

    Added template file

---
+++ src/bin/template.rs
@@ -0,0 +1,23 @@
+use std::{
+	fs::File,
+	io::{BufRead, BufReader},
+};
+
+fn main() {
+	part_one();
+	part_two();
+}
+
+fn part_one() {
+	let file = File::open("src/bin/day_0.txt").unwrap();
+	let reader = BufReader::new(file);
+
+	println!("Part One: {}", 0);
+}
+
+fn part_two() {
+	let file = File::open("src/bin/day_0.txt").unwrap();
+	let reader = BufReader::new(file);
+
+	println!("Part Two: {}", 0);
+}
